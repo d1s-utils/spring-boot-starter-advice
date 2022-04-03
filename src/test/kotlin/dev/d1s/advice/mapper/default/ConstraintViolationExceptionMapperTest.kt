a513f9ad05070719fb5a8a492d0597e5642b8014
d1snin <me@d1s.dev>
2022-04-04 00:45:09 +0300

    added tests for ConstraintViolationExceptionMapper

--- src/test/kotlin/dev/d1s/advice/mapper/default/ConstraintViolationExceptionMapperTest.kt
+++ src/test/kotlin/dev/d1s/advice/mapper/default/ConstraintViolationExceptionMapperTest.kt
@@ -1,11 +1,17 @@
 package dev.d1s.advice.mapper.default
 
+import dev.d1s.advice.domain.ErrorResponseData
 import dev.d1s.advice.mapper.defaultImpl.ConstraintViolationExceptionMapper
+import dev.d1s.teabag.testing.constant.VALID_STUB
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ContextConfiguration
 import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNull
+import javax.validation.ConstraintViolationException
 
 @SpringBootTest
 @ContextConfiguration(classes = [ConstraintViolationExceptionMapper::class])
@@ -16,6 +22,17 @@ internal class ConstraintViolationExceptionMapperTest {
 
     @Test
     fun `should map ConstraintViolationException with valid response`() {
-        expectThat(mapper)
+        val constraintViolationException = ConstraintViolationException(VALID_STUB, null)
+
+        expectThat(
+            mapper.map(constraintViolationException)
+        ) isEqualTo ErrorResponseData(HttpStatus.BAD_REQUEST, VALID_STUB)
+    }
+
+    @Test
+    fun `should map RuntimeException with null`() {
+        expectThat(
+            mapper.map(RuntimeException())
+        ).isNull()
     }
 }
