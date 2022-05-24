5c9a877f36fb5feb9c98d26809e0aa583268d1a1
d1snin <me@d1s.dev>
2022-05-24 17:29:05 +0300

    bumped to `2.0.0-stable.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "dev.d1s"
-version = "1.3.4-stable.1"
+version = "2.0.0-stable.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
