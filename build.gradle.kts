6faaa58015a8c6cd16356ec93e92e140f63f8d02
d1snin <me@d1s.dev>
2022-07-18 18:01:06 +0300

    bumped deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java-library")
     id("maven-publish")
-    id("org.springframework.boot") version "2.7.0"
+    id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
 }
 
 group = "dev.d1s"
 version = "2.0.0-stable.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
-java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -28,8 +27,8 @@ dependencies {
     api("dev.d1s.teabags:teabag-spring-web:$teabagsVersion")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
     testImplementation("dev.d1s.teabags:teabag-testing:$teabagsVersion")
     testImplementation("dev.d1s.teabags:teabag-testing-spring-web:$teabagsVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -52,7 +51,9 @@ tasks.withType<Test> {
 }
 
 tasks.withType<KotlinCompile> {
-    targetCompatibility = "11"
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Jar> {
@@ -80,5 +81,5 @@ publishing {
 }
 
 kotlin {
-    explicitApi = org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Warning
+    explicitApi()
 }
